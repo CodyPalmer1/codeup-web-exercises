ef0d55afc52733f1061efa5bf38902d239c304b3
Cody Palmer <codyryanpalmer@gmail.com>
2022-08-03 16:18:05 -0500

    itertaing arrays

--- lessonExcercises/js/break_and_continue.js
+++ lessonExcercises/js/break_and_continue.js
@@ -1,13 +1,13 @@
-//BREAK AND CONTINUE
 do {
-    var number = prompt("Please enter an odd number between 1 and 50: ");
-    for (var i = 1; i <= 50; i++) {
-        if (i % 2 !== 0 && i === Number(number)) {
-            console.log(`Here is an odd number: ${i}`);
-        } else if (i % 2 !== 0 && i === number) {
-            console.log(`Yikes! Skipping number: ${number}`)
+    var number = prompt("enter a number between 1 and 50");
+    for(var i = 1; i <= 50; i++){
+        if(i % 2 !== 0 && i === Number(number)){
+            console.log(`Yikes! Skipping number: ${i}`)
+        }else if(i % 2 !== 0) {
+            console.log(`This is an odd number: ${i}`)
         }
     }
-    while (isNaN(number) || number % 2 === 0 || number >= 50 || number < 1)
-}
+}while (isNaN(number) || number % 2 === 0 || number >= 50 || number < 1);
+
+
 
