b79667630c784f3561990b4a7e31ebbcb489d0ef
Cody Palmer <codyryanpalmer@gmail.com>
2022-09-06 10:00:59 -0500

    Updates to jQuery Exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,9 +1,9 @@
-$(function() {
-    alert( 'The DOM has finished loading!' );
-});
-
-$(function() {
-    $( 'li').css( "font-size", "20px");
-    $('h1, p, li').css("background-color", '#FFFF00');
-    // $('h1').css("alert", "h1")
-});
+// $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+// $(function() {
+//     $( 'li').css( "font-size", "20px");
+//     $('h1, p, li').css("background-color", '#FFFF00');
+//     // $('h1').css("alert", "h1")
+// });
